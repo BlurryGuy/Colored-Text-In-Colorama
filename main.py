646d8bd8ed72d68934f94073b1ce4cec01603752
Blurry <114656831+BlurryGuy@users.noreply.github.com>
2022-10-15 14:03:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from colorama import * # * means that it gets all the stuff needed out of a module
+
+print(Fore.GREEN + "I am now green text!\n" + Fore.RESET + "You resetted the color now im the standard color!\n" + Fore.BLUE + "I am now blue text!") # Coloring text, self explantory, if you need help dm "! Blurry#6286"
+
+input() # stopping the console from closing
